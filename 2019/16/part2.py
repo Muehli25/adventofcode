b7d68c8be5360738e68ee170bbbf29a027965201
Lars Mühlenstädt <muehlenstaedt.lars@gmail.com>
2021-01-19 10:06:17 +0100

    add 2019/16

---
+++ 2019/16/part2.py
@@ -0,0 +1,67 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import pathlib
+
+DIR = pathlib.Path(__file__).parent.absolute()
+
+def create_pattern(counter, size):
+    pattern = []
+    size = int(size / 4) + 1
+    for i in range(0,size):
+        for i in range(0,counter):
+            pattern.append(0)
+        for i in range(0, counter):
+            pattern.append(1)
+        for i in range(0,counter):
+            pattern.append(0)
+        for i in range(0, counter):
+            pattern.append(-1)
+    return pattern
+
+def get_num_array(input):
+    return [int(x) for x in str(input)]
+
+def get_phase(input_number):
+    num_array = get_num_array(input_number)
+    result = []
+    for i in range(len(num_array)):
+        temp_result = 0
+        pattern = create_pattern(i + 1, len(num_array))
+        pattern = pattern[1:]
+        pattern.append(0)
+        for j in range(len(num_array)):
+            pattern_pos = int(pattern[j])
+            temp_result += pattern_pos * int(num_array[j])
+        result.append(abs(temp_result) % 10)
+
+    return result
+
+def get_string_from_num(array):
+    return ''.join(map(str, array))
+
+def challenge(input):
+    result = input
+    for i in range(100):
+        temp = get_phase(result)
+        result = get_string_from_num(temp)
+        print(f'Phase {i}')
+
+    return result
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #global input
+    n = 10000
+    test1 = '03036732577212944063491565474664'*n
+    offset = int(test1[:7])
+    result = challenge(test1)
+    print(result[offset:offset+10])
+
+    with open(DIR / 'input.txt') as f:
+       input = f.read().split("\n")[0]
+    #print(input)
+    #print(challenge(input))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
