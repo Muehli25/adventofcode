80a842d2b8b68319d4cf81914e0040c7257b0cae
Lars Mühlenstädt <muehlenstaedt.lars@gmail.com>
2021-12-06 13:48:41 +0100

     🎄 Day 6 Challenge 2

--- 2021/6/challenge.py
+++ 2021/6/challenge.py
@@ -14,9 +14,9 @@ def result_challenge_2():
     with open("./data/result_test_2.txt") as file:
         return int(file.read())
 
-def part_1(input, days):
+def part_1(input):
     fishs =  [*map(int, input[0].split(","))]
-    for _ in range (0, days):
+    for _ in range (0, 80):
         new_fish = 0
         for i in range(0, len(fishs)):
             if fishs[i] == 0:
@@ -30,13 +30,27 @@ def part_1(input, days):
     return len(fishs)
 
 def part_2(input):
+    fishs =  [*map(int, input[0].split(","))]
+    
+    # The first implementation is very slow because the array is growing relly fast.
+    # since i don't care for the state of every fish i can optimize it by only looking at the fish in a specific state.
 
-    return 0
+    fish_age = [0] * 9 # a fish can only have a value in range of 0 - 8
+    
+    for i in fishs:
+        fish_age[i] += 1
+
+    for _ in range (0, 256):
+        x = fish_age[0]
+        fish_age = fish_age[1:] + [x]
+        fish_age[6] += x 
+    
+    return sum(fish_age)
 
 if __name__ == "__main__":
-    if part_1(test_data(), 80) == result_challenge_1():
+    if part_1(test_data()) == result_challenge_1():
         print("Challenge 1: Success ! 🥳")
-        print(f"Final Solution for Challenge 1: {part_1(input_data(), 80)}")
+        print(f"Final Solution for Challenge 1: {part_1(input_data())}")
     else:
         print("Challenge 1: Implementation is wrong! 😔")
 
