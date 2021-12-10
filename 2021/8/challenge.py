c3eb676eff6141c17be60bcffe64baa626e2dce8
Lars Mühlenstädt <muehlenstaedt.lars@gmail.com>
2021-12-10 23:24:22 +0100

    🎄 Day 8 Part 2

--- 2021/8/challenge.py
+++ 2021/8/challenge.py
@@ -1,3 +1,6 @@
+from numpy import number, sign
+
+
 def input_data():
     with open("./data/input.txt") as file:
         return [i.strip() for i in file.readlines()]
@@ -26,7 +29,59 @@ def part_1(input):
     return instances
 
 def part_2(input):
-    return 0
+    result_sum = 0
+    for line in input:
+        signal, output = map(str.strip, line.split("|"))
+        
+        signal = ["".join(sorted(t)) for t in signal.split()]
+        output = ["".join(sorted(t)) for t in output.split()]
+
+        letter_dictionary = dict()
+        number_dictionary = dict()
+
+        for i in range(len(signal)):
+            if len(signal[i]) == 2:
+                letter_dictionary[1] = signal[i]
+                number_dictionary[signal[i]] = 1
+            elif len(signal[i]) == 3:
+                letter_dictionary[7] = signal[i]
+                number_dictionary[signal[i]] = 7
+            elif len(signal[i]) == 4:
+                letter_dictionary[4] = signal[i]
+                number_dictionary[signal[i]] = 4
+            elif len(signal[i]) == 7:
+                letter_dictionary[8] = signal[i]
+                number_dictionary[signal[i]] = 8
+        
+        diff1_4 = "".join(set(letter_dictionary[1]).symmetric_difference(set(letter_dictionary[4])))
+
+        for i in range(len(signal)):
+            if len(signal[i]) == 6:
+                if all(map(signal[i].__contains__, diff1_4)):
+                    if all(map(signal[i].__contains__, letter_dictionary[7])):
+                        letter_dictionary[9] = signal[i]
+                        number_dictionary[signal[i]] = 9
+                    else:
+                        letter_dictionary[6] = signal[i]
+                        number_dictionary[signal[i]] = 6
+                else:
+                    letter_dictionary[0] = signal[i]
+                    number_dictionary[signal[i]] = 0
+            elif len(signal[i]) == 5:
+                if all(map(signal[i].__contains__, letter_dictionary[1])):
+                    letter_dictionary[3] = signal[i]
+                    number_dictionary[signal[i]] = 3
+                elif all(map(signal[i].__contains__, diff1_4)):
+                    number_dictionary[signal[i]] = 5
+                else:
+                    number_dictionary[signal[i]] = 2
+                
+        result = [""] * 4
+        for i in range(len(output)):
+            result[i] = str(number_dictionary[output[i]])
+
+        result_sum += int(''.join(result))
+    return result_sum
 
 if __name__ == "__main__":
     if part_1(test_data()) == result_challenge_1():
