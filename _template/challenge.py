b6548be09667232fe3b07946f3a0d957c25b534e
Lars Muehlenstaedt <lars.muehlenstaedt@msg-david.de>
2023-12-16 21:24:35 +0100

    Add template

---
+++ _template/challenge.py
@@ -0,0 +1,39 @@
+def input_data():
+    with open("./data/input.txt") as file:
+        return [i.strip() for i in file.readlines()]
+
+
+def test_data():
+    with open("./data/test.txt") as file:
+        return [i.strip() for i in file.readlines()]
+
+
+def result_challenge_1():
+    with open("./data/result_test_1.txt") as file:
+        return int(file.read())
+
+
+def result_challenge_2():
+    with open("./data/result_test_2.txt") as file:
+        return int(file.read())
+
+
+def part_1(input):
+    return 0
+
+
+def part_2(input):
+    return 0
+
+if __name__ == "__main__":
+    if part_1(test_data()) == result_challenge_1():
+        print("Challenge 1: Success ! 🥳")
+        print(f"Final Solution for Challenge 1: {part_1(input_data())}")
+    else:
+        print("Challenge 1: Implementation is wrong! 😔")
+
+    if part_2(test_data()) == result_challenge_2():
+        print("Challenge 2: Success ! 🥳")
+        print(f"Final Solution for Challenge 2: {part_2(input_data())}")
+    else:
+        print("Challenge 2: Implementation is wrong! 😔")
