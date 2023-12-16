5598214c9788d4b59526618f8470d48834cc474c
Lars Mühlenstädt <muehlenstaedt.lars@gmail.com>
2023-12-16 23:29:08 +0100

    2023 🎄 Day 2

--- 2023/2/challenge.py
+++ 2023/2/challenge.py
@@ -28,11 +28,41 @@ def result_challenge_2():
 
 
 def part_1(input):
-    return 0
+    max_red = 12
+    max_green = 13
+    max_blue = 14
 
+    result = 0
+    for line in input:
+        game_number, red, green, blue = parse_data(line)
+        if red <= max_red and green <= max_green and blue <= max_blue:
+            result = result + game_number
+    return result
 
 def part_2(input):
-    return 0
+    result = 0
+    for line in input:
+        _, red, green, blue = parse_data(line)
+        result = result + (red*green*blue)
+    return result
+
+def parse_data(line):
+    game_number = int(line.split(":")[0].split(" ")[1])
+    rounds = line.split(":")[1].split(";")
+    red, green, blue = 0,0,0
+    for round in rounds:
+        cubes = round.split(",")
+        for cube in cubes:
+            cube = cube.strip()
+            if "red" in cube:
+                red = max(int(cube.split(" ")[0]), red)
+            if "green" in cube:
+                green =  max(int(cube.split(" ")[0]), green)
+            if "blue" in cube:
+                blue = max(int(cube.split(" ")[0]), blue)
+
+    return game_number, red, green, blue
+
 
 if __name__ == "__main__":
     if part_1(test_data()) == result_challenge_1():
@@ -41,7 +71,7 @@ if __name__ == "__main__":
     else:
         print("Challenge 1: Implementation is wrong! 😔")
 
-    if part_2(test_data()) == result_challenge_2():
+    if part_2(test_data_2()) == result_challenge_2():
         print("Challenge 2: Success ! 🥳")
         print(f"Final Solution for Challenge 2: {part_2(input_data())}")
     else:
