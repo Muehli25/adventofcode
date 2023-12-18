eda0126292cbc0deaba26541e4925570444e99f7
Lars Mühlenstädt <muehlenstaedt.lars@gmail.com>
2023-12-18 23:34:36 +0100

    Day 3 Part 2

--- 2023/3/challenge.py
+++ 2023/3/challenge.py
@@ -1,5 +1,6 @@
 import os
-import re
+from enum import Enum
+from operator import mul
 
 path, _ = os.path.split(os.path.abspath(__file__))
 
@@ -29,7 +30,6 @@ def result_challenge_2():
 
 
 def part_1(input):
-    result = 0
     symbol_positions = []
     currently_number = False
     current_number = ""
@@ -75,19 +75,93 @@ def part_1(input):
     return result
 
 def part_2(input):
-    result = 0
-    return result
+    symbol_positions = []
+    currently_number = False
+    current_number = ""
+    current_number_pos = ()
+    current_state = State.FIRST
 
-def is_neighbor(pos_symbol, number):
-    list_neighbors = []
-    for i in range(len(number.value())):
-        print(i)
+    for row, line in enumerate(input):
+        line = parse_data(line)
+        for col, symbol in enumerate(line):
+            if symbol == "*":
+                symbol_positions.append((row, line.index(symbol, col)))
+    result = []
+
+    for current_symbol in symbol_positions:
+        current_state= State.FIRST
+        first_number = 0
+        second_number = 0
+        for row, line in enumerate(input):
+            line = parse_data(line)
+            for col, symbol in enumerate(line):
+                if symbol == " ":
+                    if currently_number:
+                        currently_number = False
+                        if current_state == State.FIRST:
+                            if is_neighbor([current_symbol], (current_number_pos,current_number)):
+                                first_number = int(current_number)
+                                current_state= State.SECOND
+                        elif current_state == State.SECOND:
+                            if is_neighbor([current_symbol], (current_number_pos,current_number)):
+                                second_number = int(current_number)
+                                current_state= State.CORRECT
+                        else: 
+                            if is_neighbor([current_symbol], (current_number_pos,current_number)):
+                                current_state = State.ERROR
+                                print("ERROR", first_number, second_number, current_number)
+                elif not symbol in "1234567890" and symbol == "*":
+                    if currently_number:
+                        currently_number = False
+                        if current_state == State.FIRST:
+                            if is_neighbor([current_symbol], (current_number_pos,current_number)):
+                                first_number = int(current_number)
+                                current_state= State.SECOND
+                        elif current_state == State.SECOND:
+                            if is_neighbor([current_symbol], (current_number_pos,current_number)):
+                                second_number = int(current_number)
+                                current_state= State.CORRECT
+                        else: 
+                            if is_neighbor([current_symbol], (current_number_pos,current_number)):
+                                current_state = State.ERROR
+                                print("ERROR", first_number, second_number, current_number)
+                else:
+                    if currently_number:
+                        current_number = current_number + symbol
+                    else:
+                        current_number_pos = (row, col)
+                        current_number = symbol
+                        currently_number = True
+        if current_state == State.CORRECT:
+            result.append([first_number, second_number])
+            
+    return sum(mul(*num) for num in result)
+
+def is_neighbor(symbol_positions, number):
+    allowed_pos = []
+    for symbol in symbol_positions:
+        allowed_pos.append((symbol[0]-1,symbol[1]-1))
+        allowed_pos.append((symbol[0]-1,symbol[1]))
+        allowed_pos.append((symbol[0]-1,symbol[1]+1))
+        allowed_pos.append((symbol[0],symbol[1]+1))
+        allowed_pos.append((symbol[0],symbol[1]-1))
+        allowed_pos.append((symbol[0]+1,symbol[1]-1))
+        allowed_pos.append((symbol[0]+1,symbol[1]))
+        allowed_pos.append((symbol[0]+1,symbol[1]+1))
+    (row, cols) , value = number
+    for i in range(len(value)):
+        if (row, cols + i) in allowed_pos:
+            return True
+    return False
 
 def parse_data(line):
     return line.replace(".", " ")
 
-def get_adjacent_numbers(input, x, y):
-    return y
+class State(Enum):
+    FIRST = 1
+    SECOND = 2
+    CORRECT = 3
+    ERROR = 4
 
 if __name__ == "__main__":
     if part_1(test_data()) == result_challenge_1():
