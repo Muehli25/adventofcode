13080001a81034ad8b466ccde71ef56275128181
Lars Mühlenstädt <muehlenstaedt.lars@gmail.com>
2024-12-08 12:08:52 +0100

    2024 🎄 Day 2 🎁 Part 2

--- 2024/2/challenge.py
+++ 2024/2/challenge.py
@@ -29,6 +29,20 @@ def result_challenge_2():
 
 # ------ Helper ------
 
+def check_levels(values, increasing, skip_once):
+    level_prev = values[0]
+    for value in values[1:]:
+        if increasing and 0 < value - level_prev  <= 3:
+            level_prev = value
+            continue
+        elif not increasing and 0 > value - level_prev >= -3:
+            level_prev = value
+            continue
+        elif skip_once:
+            return False
+        else:
+            skip_once = True
+    return True
 
 # ------ Parts ------
 def part_1(input):
@@ -37,11 +51,11 @@ def part_1(input):
         values = [int(x) for x in i.split(" ")]
         temp_safe = True
         # Based on the first index check if the values should decrease or increase
-        increaseing = (values[0] - values[1]) < 0
+        increasing = (values[0] - values[-1]) < 0
         for index in range(len(values) - 1):
-            if not increaseing and 0 < values[index] - values[index + 1] <= 3:
+            if not increasing and 0 < values[index] - values[index + 1] <= 3:
                 continue
-            elif increaseing and 0 > values[index] - values[index + 1] >= -3:
+            elif increasing and 0 > values[index] - values[index + 1] >= -3:
                 continue
             else:
                 temp_safe = False
@@ -51,8 +65,20 @@ def part_1(input):
     return safe
 
 
-def part_2(input):
-    return 0
+def part_2(input): 
+    safe = 0
+    for i in input:
+        values = [int(x) for x in i.split(" ")]
+        # Based on the first index check if the values should decrease or increase
+        increasing = (values[0] - values[-1]) < 0
+        test = check_levels(values, increasing, False)
+        if not test:
+            # maybe the first number was wrong
+            if check_levels(values[1:], (values[1] - values[-1]) < 0, True):
+                safe +=1
+        else:
+            safe += 1
+    return safe
 
 if __name__ == "__main__":
     if part_1(test_data()) == result_challenge_1():
